29110991508945aa142c0cf397e52f7bd1a8498c
miljana <miljanacakicmc@gmail.com>
2024-02-11 15:01:57 +0100

    statistics rutiranje

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Book } from '../models/book';
-import { environment } from 'D:/LiteraryJourney-Frontend/LiteraryJourneyApp/src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
